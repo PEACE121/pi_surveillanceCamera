c7ed78c71cc42419dd61d917762d33e1faa48358
PEACE121 <pi@raspberrypi.(none)>
2016-03-20 10:00:52 +0000

    wiringPi added

--- setup.sh
+++ setup.sh
@@ -6,6 +6,12 @@ sudo chmod +x setup.sh
 sudo ./setup.sh
 cd /home/pi/pi_surveillanceCamera 
 
+#wringPi (for light)
+cd wiringPi
+./build
+cd ..
+
+
 # pan tilt configuration
 cd panTilt
 sudo ./setup.sh
